d49a973f4105b4cf6c7180c4891d183042c4be49
smg <audrl1010@naver.com>
2016-12-29 18:34:25 +0900

    예제 추가

--- FluidStyleAutoLayoutDemo/ViewController.swift
+++ FluidStyleAutoLayoutDemo/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController
+    : UIViewController,
+    UITableViewDataSource
 {
+    // MARK: - Properties
+    lazy var tableView: UITableView = {
+        let tb = UITableView()
+        tb.dataSource = self
+        tb.register(Cell.self, forCellReuseIdentifier: Cell.cellIdentifier)
+        return tb
+    }()
+    
+    // MARK: - Set up views and constraints.
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
     }
-
-    override func didReceiveMemoryWarning()
+    
+    func setupViews()
     {
-        super.didReceiveMemoryWarning()
+        view.addSubview(tableView)
+    }
+    
+    func setupConstraints()
+    {
+        tableView
+            .fs_leftAnchor(equalTo: view.leftAnchor)
+            .fs_rightAnchor(equalTo: view.rightAnchor)
+            .fs_topAnchor(equalTo: topLayoutGuide.bottomAnchor)
+            .fs_bottomAnchor(equalTo: view.bottomAnchor)
+            .fs_endSetup()
+    }
+    
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int
+    {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cell.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row + 1)"
+        return cell
     }
 }
+
+class Cell: UITableViewCell
+{
+    class var cellIdentifier: String { return "\(Cell.self)" }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?)
+    {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+}
+
+
+
+
 
 
 
